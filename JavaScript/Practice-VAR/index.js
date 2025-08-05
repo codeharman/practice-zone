6dadbac7567eac419701cf59fa8b090ffc4aaf76
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2025-08-05 08:16:05 +0530

    practiced const, varand let variables

--- JavaScript/Practice-VAR/index.js
+++ JavaScript/Practice-VAR/index.js
@@ -13,3 +13,15 @@ harman2 = `je ai 25 ans`
 console.log(harman2);
 
 console.log(harman + ' ' + harman2);
+
+// I practiced here how to use the let keyword in JavaScript, it can be reassigned like var but not redeclared like const
+
+let count = 1;
+count = 2;
+count = 3;
+console.log(count);
+
+// const is used to decalre a constant var once, it cannot be reassigned or redeclared
+
+const name = `harman`;
+console.log(name);
