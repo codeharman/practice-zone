69af7d7b0e59dd4c9f1255cf91272958d5b30410
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2025-08-12 19:51:13 +0530

    learned the if, else and else if statment

--- JavaScript/practice_types_and_conditionals/index.js
+++ JavaScript/practice_types_and_conditionals/index.js
@@ -1,1 +1,24 @@
 // If statement is true or not, return true or false
+
+const harman = true;
+
+if (harman) {
+  console.log("harman is good");
+} else {
+  console.log("harman is bad");
+}
+
+// now we can also use else if statement 
+// we use this when we have multiple conditions to check
+
+const age = false;
+const isAdult = false;
+
+if (age) {
+  console.log("Harman is 18 years old");
+} else if (isAdult) {
+  console.log("Harman is an adult");
+} else {
+  console.log("harman is not an adult");
+}
+
