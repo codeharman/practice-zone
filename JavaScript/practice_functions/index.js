3ba7369696a48f62e7614578f8a6a8d671c748e2
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2025-08-19 13:22:11 +0530

    did some practice functions

---
+++ JavaScript/practice_functions/index.js
@@ -0,0 +1,10 @@
+// Here I learned why we need functions at the first place
+
+function echo(input, greeting) {
+  return `${greeting} ${input}`;
+}
+
+const result = echo(42, "hi");
+console.log(result);
+
+ 
