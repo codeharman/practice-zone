c3ed2326553350e6ea7fc6e612010327b56f6d72
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2025-08-23 20:44:57 +0530

    learned var, datatypes

---
+++ Python/index.py
@@ -0,0 +1,23 @@
+# the first lesson is how to print inside the python we use print
+
+print ('This is my first print')
+
+# the second thing we learned is the variables, they are like storage box
+
+firstName = 'Harman'
+lastName = 'Singh'
+
+print (firstName + ' ' + lastName)
+
+# I learned what are the different datatypes and typecasting
+
+firstdiploma = "software engineering"
+
+print(firstdiploma)
+
+# how to knwo which datatype are we talking about by using the type input
+
+print (type("hello")) #string
+print (type(2)) #integer
+print (type(True)) #boolean
+print (type(1.24)) #float 
