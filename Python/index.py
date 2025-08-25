9586f28ef00518558a76df4840fad256a524ac5b
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2025-08-25 20:32:05 +0530

    learned user input

--- Python/index.py
+++ Python/index.py
@@ -31,3 +31,26 @@ itemQuantity = 2
 
 print(str(itemName) + ' ' + str(itemPrice) + " " + str(itemQuantity))
 print(itemName, itemPrice, itemQuantity)
+
+
+#user input
+#I learned how to make input user thing in the python where one can put the input data and get the result
+
+#I made the calculator
+
+num1=input('Enter a digit: ')
+num2=input('Enter a second numnber: ')
+answer=float(num1)+float(num2)
+print(answer)
+
+# another example of making the etre conjucation
+
+frenchWord = input('Entre the word: ')
+answer1 = "Je " + str(frenchWord)
+answer2 = "tu " + str(frenchWord)
+answer3 = "Il/elle " + str(frenchWord)
+answer4 = "Nous " + str(frenchWord)
+answer5 = "Vous " + str(frenchWord)
+answer6 = "Ils/elles" + str(frenchWord)
+
+print(answer1, answer2, answer3, answer4, answer5, answer6)
