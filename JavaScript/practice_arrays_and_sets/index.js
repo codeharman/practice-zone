da7b7462441733ce225fabd2919b7ca7b263978e
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2025-09-17 19:09:15 +0530

    I learned how to use the indexOf, includes and some

--- JavaScript/practice_arrays_and_sets/index.js
+++ JavaScript/practice_arrays_and_sets/index.js
@@ -20,7 +20,7 @@ console.log(toDos);
 
  console.log(toDos);
 
- // this shows that objects are good when we know somehting but not when we don't know
+// this shows that objects are good when we know somehting but not when we don't know
 // array stores the order of things
 // const a = []; this is array
 // const b = {}; this is object
@@ -43,6 +43,16 @@ console.log(temp[1]); // tells us what temperature by putting the index number
 
 // but what if we put the number which doesn't exist
 
-console.log(temp.indexOf(49)); // here -1 means the number doesn't exist
+console.log(temp.indexOf(49) > -1); // here -1 means the number doesn't exist
+// but the better version is includes
 
+console.log(temp.includes(50));
 
+// but what if there is more items inside the arrays
+
+const newTemp = [
+  {temp: 99, isRecordHigh: true}, // please note I wrote obj here
+  {temp: 70, isRecordHigh: false}
+];
+
+console.log(newTemp.some(newTemp => newTemp.isRecordHigh === true)); // .some tells us if there is one thing that is matching inside a given containers
