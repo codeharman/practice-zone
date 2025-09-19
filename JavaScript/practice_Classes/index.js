4a236bc67fa0a75403a93e4f737c339713042520
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2025-09-19 14:54:34 +0530

    I learned how to use the extend, super for the classes

--- JavaScript/practice_Classes/index.js
+++ JavaScript/practice_Classes/index.js
@@ -62,3 +62,70 @@ class Finals {
 
 const final = new Finals(1, "harman", 'math');
 console.log(final.addSubject)
+
+// Now I learned the share class features with extends
+
+class Product {
+  constructor(name, price, discountable) {
+    this.name = name;
+    this.price = price;
+    this.discountable = discountable;
+  }
+
+  isDiscountable() {
+    return this.discountable;
+  }
+};
+
+// extends is used to inherit the methods and obj from the given classes
+
+class SaleProduct extends Product {
+  constructor(name, price, discountable, percentOff) {
+// super() Think of super as a keyword that lets the child class talk to its parent class.
+    super(name, price, discountable);
+    this.price = percentOff; 
+  }
+
+  getSalePrice() {
+    if (super.isDiscountable()) {
+      return this.price * ((100 - this.percentOff));
+    } else {
+      return `${this.name} is not eligible`
+    }
+  }
+}
+
+const p = new SaleProduct('coffee maker', 99, true, 10);
+console.log(p)
+
+// CHATGPT exercise Basic Inheritance
+
+class Vehicle {
+  constructor(brand, wheels) {
+    this.brand = brand;
+    this.wheels = wheels;
+  }
+  // method
+  describe() {
+    return `This vehicel is a ${this.brand} with ${this.wheels} wheels`
+  }
+};
+
+class Cars extends Vehicle {
+  constructor (brand, wheels, fuelType) {
+    super(brand, wheels)
+    this.fuelType = fuelType;
+  }
+
+  describeFuel() {
+    return `This car runs on ${this.fuelType}`
+  }
+}
+
+const carType = new Cars('BMW', 4, 'electric');
+
+console.log(carType)
+
+console.log(carType.describe());      // This vehicle is a BMW with 4 wheels
+console.log(carType.describeFuel());
+
