835e82eed5796678f6f7d7fefdff220bf8a8326e
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2025-09-19 19:59:07 +0530

    I learned the fetch()

--- JavaScript/practice_Async/index.js
+++ JavaScript/practice_Async/index.js
@@ -1,1 +1,21 @@
-console.log(document.body);
+// Make network reqest with fetch()
+
+// API - Application Programming Interface
+
+// CRUD
+// Create - Post
+// Read - Get
+// update - Put / Patch
+// delete - Delete
+
+// http://jsonplaceholder.typicode.com/posts
+// JSON - JavaScript Object Notation
+// fetch(url)
+//  jsonplaceholder.typicode.com
+
+// so lets get started 
+
+const h = fetch('https://jsonplaceholder.typicode.com/posts/1');
+console.log(h) // it will give you the Promise
+  .then(response => response.json())
+  .then(data => console.log(data.title))
