b6f0c3afe0b680ca6a0b9d5333dbebe92e94c2ca
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2025-09-20 14:07:31 +0530

    I learned the geoloaction and the async

--- JavaScript/practice_Async/index.js
+++ JavaScript/practice_Async/index.js
@@ -1,21 +1,8 @@
-// Make network reqest with fetch()
+// The Problem with the Callbacks
 
-// API - Application Programming Interface
+navigator.geolocation.getCurrentPosition(position =>
+{console.log(position)}
+);
 
-// CRUD
-// Create - Post
-// Read - Get
-// update - Put / Patch
-// delete - Delete
-
-// http://jsonplaceholder.typicode.com/posts
-// JSON - JavaScript Object Notation
-// fetch(url)
-//  jsonplaceholder.typicode.com
-
-// so lets get started 
-
-const h = fetch('https://jsonplaceholder.typicode.com/posts/1');
-console.log(h) // it will give you the Promise
-  .then(response => response.json())
-  .then(data => console.log(data.title))
+// The below code give output first before the upper one
+console.log('done'); 
